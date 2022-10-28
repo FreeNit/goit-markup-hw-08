c13a3d74e1a1d2c01f031fc9880b9aeab0a3975a
Dmyto Kovach <bolilui.dmytro@gmail.com>
2022-10-28 16:17:01 +0300

    Add styles and logic for the open-close modal window (mobile resolution). Potentially required debug and rework.

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
+  const logo = document.querySelector('[data-header-logo]');
 
   menuBtnRef.addEventListener('click', () => {
     const expanded =
@@ -10,5 +11,7 @@
     menuBtnRef.setAttribute('aria-expanded', !expanded);
 
     mobileMenuRef.classList.toggle('is-open');
+    logo.classList.toggle('is-logoVisible');
+    document.body.classList.toggle('modal-open');
   });
 })();
